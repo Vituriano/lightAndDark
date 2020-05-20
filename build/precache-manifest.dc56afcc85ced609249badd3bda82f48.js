1bf4cc7587328f2cf8aaa7ba0ad0e00a56439d99
Vituriano <viturianooliveira@gmail.com>
2020-05-20 00:58:01 -0300

    feature(master): add a production build

---
+++ build/precache-manifest.dc56afcc85ced609249badd3bda82f48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bab462cbe9a124225b7ee01b59206e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e444fb93b7fbbecfbaab",
+    "url": "/static/js/2.60ed9a07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60ed9a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7367a2e55690739dbd90",
+    "url": "/static/js/main.a39faf8c.chunk.js"
+  },
+  {
+    "revision": "7b125b3f8bdee6e9a09b",
+    "url": "/static/js/runtime-main.fc43044e.js"
+  }
+]);
