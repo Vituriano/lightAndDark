0c38f6599abcfc7ef0c088023be477349c6f3b80
Vituriano <viturianooliveira@gmail.com>
2020-05-20 01:05:15 -0300

    Updates

---
+++ precache-manifest.64321d89d9d5ada3a102f7d0666bf401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fc11e35e0519178799ce1cd5b9bd49",
+    "url": "/lightAndDark/index.html"
+  },
+  {
+    "revision": "19d1f45429cb43a4b9a9",
+    "url": "/lightAndDark/static/js/2.616141d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lightAndDark/static/js/2.616141d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7367a2e55690739dbd90",
+    "url": "/lightAndDark/static/js/main.a39faf8c.chunk.js"
+  },
+  {
+    "revision": "6f7aabda8fc6d16aa718",
+    "url": "/lightAndDark/static/js/runtime-main.17fc43c1.js"
+  }
+]);
